fa6ec8104f2d2198315389626830feb14c9630e6
rbrahinets <rostyslav.brahinets@gmail.com>
2022-07-26 13:13:17 +0300

    Add drawing moving cells

--- game/scripts/script.js
+++ game/scripts/script.js
@@ -7,9 +7,9 @@ let fieldOfGame = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,7 +27,11 @@ function drawField() {
     let mainInnerHTML = "";
     for (let y = 0; y < fieldOfGame.length; y++) {
         for (let x = 0; x < fieldOfGame[y].length; x++) {
-            mainInnerHTML += "<div class='cell'></div>>";
+            if (fieldOfGame[y][x] === 1) {
+                mainInnerHTML += "<div class='cell movingCell'></div>";
+            } else {
+                mainInnerHTML += "<div class='cell'></div>";
+            }
         }
     }
     main.innerHTML = mainInnerHTML;
