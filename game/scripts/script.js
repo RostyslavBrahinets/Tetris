51db2a06565062f9c05937075209bdedc7899547
rbrahinets <rostyslav.brahinets@gmail.com>
2022-07-26 13:43:37 +0300

    Add moving figure down

--- game/scripts/script.js
+++ game/scripts/script.js
@@ -1,15 +1,15 @@
 let main = document.querySelector(".main");
 
 let fieldOfGame = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,6 +22,8 @@ let fieldOfGame = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+
+let gameSpeed = 700;
 
 function drawField() {
     let mainInnerHTML = "";
@@ -37,4 +39,38 @@ function drawField() {
     main.innerHTML = mainInnerHTML;
 }
 
+function canFigureMoveDown() {
+    for (let y = 0; y < fieldOfGame.length; y++) {
+        for (let x = 0; x < fieldOfGame[y].length; x++) {
+            if (fieldOfGame[y][x] === 1) {
+                if (y === fieldOfGame.length - 1) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+function moveFigureDown() {
+    if (canFigureMoveDown()) {
+        for (let y = fieldOfGame.length - 1; y >= 0; y--) {
+            for (let x = 0; x < fieldOfGame[y].length; x++) {
+                if (fieldOfGame[y][x] === 1) {
+                    fieldOfGame[y + 1][x] = 1;
+                    fieldOfGame[y][x] = 0;
+                }
+            }
+        }
+    }
+}
+
+function startGame() {
+    moveFigureDown()
+    drawField()
+    setTimeout(startGame, gameSpeed);
+}
+
 drawField()
+setTimeout(startGame, gameSpeed);
